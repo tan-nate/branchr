da47164273c2ab1c80530f33066926ab5c9c9591
Nate Tan <nktan93@gmail.com>
2019-06-14 18:52:14 +0000

    edited models typo

--- app/models/mash.rb
+++ app/models/mash.rb
@@ -2,5 +2,5 @@ class Mash < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :halves, class_name: "Mash", foreign_key: "whole_id"
-  belongs_to :whole, class_name: "Employee"
+  belongs_to :whole, class_name: "Mash"
 end
