d54aee6e5a6d336382c182f7d90fbc8d87253360
Nate Tan <nktan93@gmail.com>
2019-06-15 17:55:45 +0000

    working on branch hash

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -2,4 +2,18 @@ class Branch < ActiveRecord::Base
   belongs_to :tree
   has_many :children, class_name: "Branch", foreign_key: "parent_id"
   belongs_to :parent, class_name: "Branch"
+
+  def self.branch_hash
+    hash = {}
+
+    branches = Branch.all
+    branches.each do |branch|
+      if branch.parent
+        hash[branch.parent.name] = {}
+        hash[branch.parent.name][branch.name] = {}
+      end
+    end
+
+    hash
+  end
 end
