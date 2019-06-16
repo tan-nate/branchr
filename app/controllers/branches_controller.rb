a72568b82d4e65a1ba9359b27d70849a4d96bd79
Nate Tan <nktan93@gmail.com>
2019-06-16 04:22:30 +0000

    working on new view

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -4,19 +4,22 @@ class BranchesController < ApplicationController
   end
 
   get '/branches/new' do
-    if logged_in?
-      @tree = Lister.list(Branch.all)
-      erb :'branches/new'
-    else
-      redirect '/login'
-    end
+    # if logged_in?
+    #   @tree = Lister.list(Branch.all)
+    #   erb :'branches/new'
+    # else
+    #   redirect '/login'
+    # end
+
+    @tree = Lister.list(Branch.all)
+    erb :'branches/new'
   end
 
-  post '/branches' do
-    # binding.pry
-    child1 = Branch.find_or_create_by(name: params[:child1])
-    child2 = Branch.find_or_create_by(name: params[:child2])
-    parent = Branch.find_or_create_by(name: params[:parent])
+  post '/branches/new' do
+    #binding.pry
+    child1 = Branch.find_or_create_by(name: params[:child1].downcase.match(/[a-z]+/).to_s)
+    child2 = Branch.find_or_create_by(name: params[:child2].downcase.match(/[a-z]+/).to_s)
+    parent = Branch.find_or_create_by(name: params[:parent].downcase.match(/[a-z]+/).to_s)
     parent.children << child1
     parent.children << child2
 
