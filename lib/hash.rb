521ebe48db87f11c982a05e43be3088274457e44
Nate Tan <nktan93@gmail.com>
2019-06-16 04:35:16 +0000

    styling

--- lib/hash.rb
+++ lib/hash.rb
@@ -2,7 +2,7 @@ class Hash
   # from https://gist.github.com/chastell/1196800
   def to_html
     [
-      '<ul>',
+      '<ul class="tree">',
       map { |k, v| ["<li><strong>#{k.name}</strong>", v.respond_to?(:to_html) ? v.to_html : "<span>#{v.name}</span></li>"] },
       '</ul>'
     ].join
