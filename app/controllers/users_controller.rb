58cf7a613f532a38be5ff7db912553af7c795258
Nate Tan <nktan93@gmail.com>
2019-06-21 17:59:27 +0000

    adding current_user action to multiple routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
+    @current_user = current_user
     erb :'users/show'
   end
 
