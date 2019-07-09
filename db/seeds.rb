332296a12d3022a216e171a5f7fa009915ae2d53
Nate Tan <nktan93@gmail.com>
2019-07-09 16:59:47 +0000

    Revert "updated seeds"
    This reverts commit f2eed61a55561454ae8897894a14e43c1a396b7e.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@ room = Branch.create(name: "room", tree: furniture)
 
 sofa = Branch.create(name: "sofa", tree: furniture)
 table = Branch.create(name: "table", tree: furniture)
-sofa.parent = room
-table.parent = room
+room.children << sofa
+room.children << table
 
 chair = Branch.create(name: "chair", tree: furniture)
 couch = Branch.create(name: "couch", tree: furniture)
-chair.parent = sofa
-couch.parent = sofa
+sofa.children << chair
+sofa.children << couch
 
 legs = Branch.create(name: "legs", tree: furniture)
 surface = Branch.create(name: "surface", tree: furniture)
-legs.parent = table
-surface.parent = table
+table.children << legs
+table.children << surface
 
 # COMMUNITY TREE
 
@@ -27,15 +27,15 @@ room2 = Branch.create(name: "room", tree: community)
 
 sofa2 = Branch.create(name: "sofa", tree: community)
 table2 = Branch.create(name: "table", tree: community)
-sofa2.parent = room2
-table2.parent = room2
+room2.children << sofa2
+room2.children << table2
 
 chair2 = Branch.create(name: "chair", tree: community)
 couch2 = Branch.create(name: "couch", tree: community)
-chair2.parent = sofa2
-couch2.parent = sofa2
+sofa2.children << chair2
+sofa2.children << couch2
 
 legs2 = Branch.create(name: "legs", tree: community)
 surface2 = Branch.create(name: "surface", tree: community)
-legs2.parent = table2
-surface2.parent = table2
+table2.children << legs2
+table2.children << surface2
